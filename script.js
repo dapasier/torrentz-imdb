036b49b4f46e44ebe73e7289d3e923823fcf742d
Max Huttunen <max.huttunen@gmail.com>
2014-12-17 23:31:53 +0200

    updated UI

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-var pathName = location.pathname.replace(/\//g, "");
+var pathName = location.pathname.replace(/\//g, '');
 if(pathName.match(/search/)) {
-	var items = document.querySelectorAll(".results dl");
+	var items = document.querySelectorAll('.results dl');
 	
 	for(var i = 0; i < items.length; i++) {
 		(function() {
 			var item = items[i];
-			var titleLink = item.querySelector("dt a");
-			if(titleLink && item.querySelector("dt").innerHTML.match(/movies/)) {
-				var parts = titleLink.innerHTML.replace(/<[^>]*>/g, "").match(/(.+?)([0-9]{4})/);
+			var titleLink = item.querySelector('dt a');
+			if(titleLink && item.querySelector('dt').innerHTML.match(/movies/)) {
+				var parts = titleLink.innerHTML.replace(/<[^>]*>/g, '').match(/(.+?)([0-9]{4})/);
 				if(parts) {
 					var title = parts[1];
 					var year = parts[2];
 					
 					var r = new XMLHttpRequest;
-					r.open("GET", 'http://www.omdbapi.com/?t=' + title + '&y=' + year, true);
+					r.open('GET', 'http://www.omdbapi.com/?t=' + title + '&y=' + year, true);
 					r.onload = function () {
 						var result = JSON.parse(r.responseText);
-						if(result.Response !== "False") {
+						if(result.Response !== 'False') {
 							var rating = result.imdbRating;
-							if(!isNaN(rating))
-								item.querySelector("dt").innerHTML += '<a title="' + result.Runtime + '" href="http://www.imdb.com/title/' + result.imdbID + '" target="_blank"><div style="background-color:hsl(' + rating * 12 + ', 90%, 50%);" class="imdb-rating">' + rating + '</div></a>';
+							if(!isNaN(rating)) {
+								var dd = item.querySelector('dd');
+								var dt = item.querySelector('dt');
+
+								var span = document.createElement('span');
+								span.setAttribute('class', 'imdb');
+								span.innerHTML = '<a title="' + result.Runtime + '" href="http://www.imdb.com/title/' + result.imdbID + '" target="_blank"><div style="background-color:hsl(' + rating * 12 + ', 90%, 45%);" class="imdb-rating">' + rating + '</div></a>';
+								dd.querySelector('.v').insertAdjacentElement('beforeBegin', span);
+
+								var spanWidth = parseInt(window.getComputedStyle(span).width);
+								dd.style.width = (parseInt(window.getComputedStyle(dd).width) + spanWidth) + 'px';
+								dt.style.width = (parseInt(window.getComputedStyle(dt).width) - spanWidth / 2) + 'px';
+							}
 						}
 					};
 					r.send();
